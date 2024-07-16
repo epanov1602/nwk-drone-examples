2438ae87e5d24a89a281a686383094e399ce914b
Gene Panov <gene.panov@gmail.com>
2024-07-15 23:31:29 -0400

    Better exercise for not losing track

--- chaseplane_main.py
+++ chaseplane_main.py
@@ -2,14 +2,18 @@ import cv2
 from ultralytics import YOLO
 import detection
 
-# what kind of objects can we detect?
+# some settings to play with, the goal is to not lose track -- can you tune them?
+MAX_FRAMES_WITHOUT_TRACKER_OBJECT_OBSERVED = 40
+VALID_OBJECT_CLASSES = {"airplane", "bird"}
+
+# which video to download
+downloaded = detection.download_video("https://www.youtube.com/watch?v=IwrfLEw_aTw")
+
+
+# which tools do we use for detection and tracking?
 model = YOLO("resources/yolov8s.pt")  # model to detect common objects like "person", "car", "cellphone" (see "COCO")
 tracker = detection.create_vit_tracker()
 
-# if we see more than 40 frames without tracker object, consider it lost
-MAX_FRAMES_WITHOUT_TRACKER_OBJECT = 40
-
-downloaded = detection.download_video("https://www.youtube.com/watch?v=4lqVYtuOR84")
 
 camera = cv2.VideoCapture(downloaded)
 assert camera.isOpened(), "cannot read from downloaded file"
@@ -26,22 +30,32 @@ while True:
         continue
 
     frame_count += 1
+    if frame_count < 1600:
+        continue  # skip the first 1600 frames of the video
+    frame = cv2.resize(frame, (1920 // 2, 1080 // 2))
+
     x, y, w, h = None, None, None, None
 
     if tracking:
         x, y, w, h = detection.update_tracker(tracker, frame, lowest_allowed_score=0.4)
         if x is None:
-            if frame_count > time_last_seen + MAX_FRAMES_WITHOUT_TRACKER_OBJECT:
+            if frame_count > time_last_seen + MAX_FRAMES_WITHOUT_TRACKER_OBJECT_OBSERVED:
                 tracking = False
 
+    if frame_count > time_last_seen + 2:
+        warning_text = f"not seen for {frame_count - time_last_seen} frames"
+        cv2.putText(frame, warning_text, (50, 30), cv2.FONT_HERSHEY_PLAIN, fontScale=1.25, color=(0, 200, 0), thickness=1)
     cv2.imshow("video", frame)
     key = cv2.waitKey(1) & 0xFF
 
     if not tracking or key == ord(' '):
-        x, y, w, h = detection.detect_yolo_object(model, frame, valid_classnames={"airplane"})
+        x, y, w, h = detection.detect_yolo_object(model, frame, valid_classnames=VALID_OBJECT_CLASSES)
         cv2.imshow("video", frame)
         key = cv2.waitKey(1) & 0xFF
         if x is None:
+            cv2.putText(frame,
+                        f"re-detection failed (classes: {VALID_OBJECT_CLASSES}), please select manually", (50, 60),
+                        cv2.FONT_HERSHEY_PLAIN, fontScale=1.25, color=(0, 200, 0), thickness=1)
             x, y, w, h = cv2.selectROI("video", frame, False)
         if x is not None:
             tracker.init(frame, (x, y, w, h))
